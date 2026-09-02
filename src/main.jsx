74a0a269ba22da7bbe3338aefcf29bda1be53e95
lokrajbhatta361-cmyk <lokrajbhatta361@gmail.com>
2026-09-02 13:31:59 +1000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { BookingProvider } from "./context/BookingContext";
-import App from "./App.jsx";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BookingProvider } from './context/BookingContext';
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
-      <BookingProvider>
-        <App />
-      </BookingProvider>
-    </BrowserRouter>
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BookingProvider>
+      <App />
+    </BookingProvider>
+  </React.StrictMode>
 );
