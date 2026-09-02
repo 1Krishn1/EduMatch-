1d143bc91eb77f5ce11ead09fc5434191cab9c01
lokrajbhatta361-cmyk <lokrajbhatta361@gmail.com>
2026-09-02 14:10:20 +1000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Mentors from './pages/Mentors';
 import About from './pages/About';
@@ -8,14 +7,12 @@ import MyBookings from './pages/MyBookings';
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/mentors" element={<Mentors />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/book" element={<BookSession />} />
-        <Route path="/my-bookings" element={<MyBookings />} />
-      </Routes>
-    </Router>
+    <div style={{ padding: '20px' }}>
+      <Home />
+      <Mentors />
+      <About />
+      <BookSession />
+      <MyBookings />
+    </div>
   );
 }
