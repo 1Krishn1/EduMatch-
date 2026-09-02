70f7be6ac555bdd58e69a9cd41a07b1329133787
Krishna Bahadur Budhathoki <1Krishn1@users.noreply.github.com>
2026-09-02 11:20:10 +1000

    Restore EduMatch App routes and layout for Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { BookingProvider } from './context/BookingContext'
 
-// LOKESH: later wrap App with <BookingProvider> from ./context/BookingContext.jsx
-
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BookingProvider>
       <App />
-    </BrowserRouter>
-  </StrictMode>
-);
+    </BookingProvider>
+  </React.StrictMode>,
+)
